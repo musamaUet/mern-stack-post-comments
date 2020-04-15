cc88f5c7cb50262d43d6d43d068e85e8950f5972
Muhammad Usama Rabani <2016cs507@student.uet.edu.pk>
2020-04-15 16:22:15 +0500

    DB Cloud

---
+++ DB.js
@@ -0,0 +1,4 @@
+module.exports = {
+    //DB: 'mongodb://localhost:27017/mern_youtube'
+    DB: 'mongodb+srv://Muhammad:Muhammad.123@cluster0-8jc2d.mongodb.net/test?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const path = require('path');
+const config = require('./DB.js');
 
 const app = express();
 const PORT = process.env.PORT || 8080; // Step 1
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 8080; // Step 1
 const routes = require('./routes/api');
 
 // Step 2
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern_youtube', {
+mongoose.connect(process.env.MONGODB_URI || config.DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
